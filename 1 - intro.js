45998f60085c9023e43437da91a00aa22cd08d69
UJJAL DEY <deysumpa995@gmail.com>
2025-01-19 17:26:43 +0530

    First commit

---
+++ 1 - intro.js
@@ -0,0 +1,9 @@
+const number = 12;
+
+if(number < 12) {
+    console.log('Number is less than 12');
+} else {
+    console.log('Number is greater than or equal to 12');
+}
+
+console.log(`Hey this is my first code!!!`);
